e48e3c2edbb2f04367260f684b2b53c5939c2b20
yveskalume <yvkalume@gmail.com>
2025-02-15 20:38:00 +0200

    use error stdout

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|error| {
-        println!("Arguments Error: {}",error);
+        eprintln!("Arguments Error: {}",error);
         process::exit(1)
     });
     println!("Searching for '{}'", config.query);
     println!("In file {}", config.filename);
 
     if let Err(e) = terminal::run(config) {
-        println!("Application Error: {}",e)
+        eprintln!("Application Error: {}",e)
     };
 }
